124400d99e1444599ba850ed20debeb38f2bf91a
Chottek <49697145+Chottek@users.noreply.github.com>
2021-03-17 16:17:35 +0100

    Removed unnecessary imports

--- Lab02 - Selenium/GmotoTest.java
+++ Lab02 - Selenium/GmotoTest.java
@@ -1,12 +1,10 @@
 package pl.fox.seleniumlab;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
